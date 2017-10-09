34f68918af57b77b63b23dcd2b3c030aed66f3c5
stefanvuk <stefanvuk@me.com>
2017-10-09 16:07:55 +0200

    Add files via upload

---
+++ ScrollView/ScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollView
+//
+//  Created by Stefan on 1/6/17.
+//  Copyright © 2017 Stefan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
